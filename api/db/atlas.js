d0a2d0669e0030391718c1109d9da501a10c7929
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-22 07:20:28 -0500

    feat: :sparkles: Connection to the project database
    the mongodb library is used to create the database connection and the project api is used to manage and manipulate the database data.

---
+++ api/db/atlas.js
@@ -0,0 +1,18 @@
+import { loadEnv } from "vite";
+import { MongoClient } from "mongodb";
+
+const env = loadEnv("development", process.cwd(), 'NODE');
+
+export async function con() {
+    try {
+        const uri = `mongodb+srv://${env.NODE_ATLAS_USER}:${env.NODE_ATLAS_PASSWORD}@cluster0.ayssvt3.mongodb.net/${env.NODE_ATLAS_DB}`;
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        const client = await MongoClient.connect(uri, options);
+        return client.db();
+    } catch (error) {
+        return {status: 500, message: error};
+    }
+}
