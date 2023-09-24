e6f1fcfd75aad68a6b1103d1440a964a60c94c7b
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-24 09:15:30 -0500

    feat: :sparkles: decrease creation function
    The decrease function is created to subtract the auto-incrementables in case of a data injection error in the database.

--- api/db/autoincrement.js
+++ api/db/autoincrement.js
@@ -1,12 +1,21 @@
 import { con } from "./atlas.js";
 
-let db= await con();
+let db = await con();
 let autoincrement = db.collection("autoincrement");
 
 export async function  increment(coleccion){
     const sequenceDocument = await autoincrement.findOneAndUpdate(
         { _id: `${coleccion}Id` },
         { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.sequence_value;
+}
+
+export async function  decrease(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: -1 } },
         { returnDocument: "after" }
     );
     return sequenceDocument.sequence_value;
