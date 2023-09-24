059f4be4b0e51dcab0d92109ccd03c58e2c34a3d
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-24 11:04:42 -0500

    feat: :safety_vest: Validation rules for the endpoint: Area
    Validation rules are created to avoid the entry of incorrect data in the database collection management paths.

---
+++ api/controllers/vArea.js
@@ -0,0 +1,16 @@
+import { body } from "express-validator";
+
+export const vArea = [
+
+    body("name_Area")
+    .notEmpty().withMessage("name_Area must not be empty")
+    .isString().withMessage("name_Area must be a string")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]+$/).withMessage("name_Area must only contain alphanumeric characters, spaces and specific accented characters.")
+    .isLength({ max: 45 }).withMessage("name_Area must have a maximum of 45 characters"),
+
+    body("description_Area")
+    .notEmpty().withMessage("description_Area must not be empty")
+    .isString().withMessage("descripcion_Area must be a string")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.\s]+$/).withMessage("description_Area must only contain alphanumeric characters, spaces and specific accented characters.")
+    .isLength({ max: 600 }).withMessage("description_Area must have a maximum of 600 characters")
+]
