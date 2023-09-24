0da41a318e8c1c6c1dc8c6e6746b12d7bf8f2b89
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-24 15:06:35 -0500

    perf: :zap: implementation of validation rules for the endpoint and performance improvement in the validation function: Category_Inc
    The proxyV function is created to execute the verification of the input data using the validation rules, it accepts as input data the validation rules and also the mapping of the input data, this reduces the code and facilitates the data validation.

--- api/middleware/proxyPEndpoints.js
+++ api/middleware/proxyPEndpoints.js
@@ -2,45 +2,55 @@ import  express from 'express';
 import { validationResult } from 'express-validator';
 import { vArea } from '../controllers/vArea.js';
 import { vClassroom } from '../controllers/vClassroom.js';
+import { vCategory_Inc } from '../controllers/vCategory_Inc.js';
+
+//proxy used to validate the input data methods put and post methods
+
+const proxyV = (validationRule, mapping) => async (req, res, next) => {
+
+    await Promise.all(validationRule.map(field => field.run(req)));
+
+    const error = validationResult(req);
+    if(!error.isEmpty()) return res.status(400).json(error)
+
+    const mappedData = {};
+    for (const [key, value] of Object.entries(mapping)) {
+        mappedData[value] = req.body[key];
+    }
+        
+    req.body = mappedData;
+    next();
+};
+
 
 const proxyArea= express();
 const proxyClassroom = express();
+const proxyCategory_Inc = express();
 
-//proxy used to validate the input data methods put and post methods in Area
-proxyArea.use(vArea, async(req, res, next)=>{
-    try {
-        const error = validationResult(req);
-        if(!error.isEmpty()) return res.status(400).json(error)
-        let { 
-            name_Area:A_Name, description_Area:A_Description
-        } = req.body
-        req.body = {
-            A_Name, A_Description
-        }
-        next();
-    } catch (err) {
-        res.status(err.status).send(err);
-    }
-});
+// Define the mappings
 
-//proxy used to validate the input data methods put and post methods in Area
-proxyClassroom.use(vClassroom, async(req, res, next)=>{
-    try {
-        const error = validationResult(req);
-        if(!error.isEmpty()) return res.status(400).json(error)
-        let { 
-            name_Classroom:Sln_Name, description_Classroom:Sln_Description, classroom_Area: Sln_Areas
-        } = req.body 
-        req.body = {
-            Sln_Name, Sln_Description, Sln_Areas
-        }
-        next();
-    } catch (err) {
-        res.status(err.status).send(err);
-    }
-});
+const areaMapping = {
+    name_Area: 'A_Name',
+    description_Area: 'A_Description'
+};
+
+const classroomMapping = {
+    name_Classroom: 'Sln_Name',
+    description_Classroom: 'Sln_Description',
+    classroom_Area: 'Sln_Areas'
+};
+
+const category_IncMapping = {
+    name_Category_Inc: 'Cat_Name',
+    description_Category_Inc: 'Cat_Description'
+};
+
+proxyArea.use(proxyV(vArea, areaMapping));
+proxyClassroom.use(proxyV(vClassroom, classroomMapping));
+proxyCategory_Inc.use(proxyV(vCategory_Inc, category_IncMapping));
 
 export {
     proxyArea,
-    proxyClassroom
+    proxyClassroom,
+    proxyCategory_Inc
 }
