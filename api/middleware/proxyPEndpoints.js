49bc9a8ed18f32190d9588b0db7f71ad5a4b28d7
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-24 12:42:06 -0500

    feat: :safety_vest: implementation of validation rules for the endpoint: Classroom
    The proxy function is created to execute the verification of the input data using the validation rules of this endpoint.

--- api/middleware/proxyPEndpoints.js
+++ api/middleware/proxyPEndpoints.js
@@ -1,12 +1,12 @@
 import  express from 'express';
 import { validationResult } from 'express-validator';
 import { vArea } from '../controllers/vArea.js';
-
+import { vClassroom } from '../controllers/vClassroom.js';
 
 const proxyArea= express();
+const proxyClassroom = express();
 
-//proxy usado para validar los metodos datos de entrada de los metodos put y post en alimentos
-
+//proxy used to validate the input data methods put and post methods in Area
 proxyArea.use(vArea, async(req, res, next)=>{
     try {
         const error = validationResult(req);
@@ -23,6 +23,24 @@ proxyArea.use(vArea, async(req, res, next)=>{
     }
 });
 
+//proxy used to validate the input data methods put and post methods in Area
+proxyClassroom.use(vClassroom, async(req, res, next)=>{
+    try {
+        const error = validationResult(req);
+        if(!error.isEmpty()) return res.status(400).json(error)
+        let { 
+            name_Classroom:Sln_Name, description_Classroom:Sln_Description, classroom_Area: Sln_Areas
+        } = req.body 
+        req.body = {
+            Sln_Name, Sln_Description, Sln_Areas
+        }
+        next();
+    } catch (err) {
+        res.status(err.status).send(err);
+    }
+});
+
 export {
-    proxyArea
+    proxyArea,
+    proxyClassroom
 }
