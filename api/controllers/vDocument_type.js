686841889bebe71c832a2567a8645476e696359f
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-24 16:57:16 -0500

    feat: :safety_vest: Validation rules for the endpoint: Document_type
    Validation rules are created to avoid the entry of incorrect data in the database collection management paths.

---
+++ api/controllers/vDocument_type.js
@@ -0,0 +1,16 @@
+import { body } from "express-validator";
+
+export const vDocument_type = [
+
+    body("name_Document")
+    .notEmpty().withMessage("name_Document must not be empty")
+    .isString().withMessage("name_Document must be a string")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]+$/).withMessage("name_Document must only contain alphanumeric characters, spaces and specific accented characters.")
+    .isLength({ max: 45 }).withMessage("name_Document must have a maximum of 45 characters"),
+
+    body("abbreviation_Document")
+    .notEmpty().withMessage("abbreviation_Document must not be empty")
+    .isString().withMessage("abbreviation_Document must be a string")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.\s]+$/).withMessage("abbreviation_Document must only contain alphanumeric characters, spaces and specific accented characters.")
+    .isLength({ max: 10 }).withMessage("abbreviation_Document must have a maximum of 10 characters")
+]
