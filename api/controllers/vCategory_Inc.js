a4278345bda55656753d6662e6e90beb437fdfc6
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-24 21:25:59 -0500

    feat: :safety_vest: improved validation of text in the validation rules
    validation is implemented to accept characters normally used in English grammar.

--- api/controllers/vCategory_Inc.js
+++ api/controllers/vCategory_Inc.js
@@ -5,12 +5,12 @@ export const vCategory_Inc = [
     body("name_Category_Inc")
     .notEmpty().withMessage("name_Category_Inc must not be empty")
     .isString().withMessage("name_Category_Inc must be a string")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]+$/).withMessage("name_Category_Inc must only contain alphanumeric characters, spaces and specific accented characters.")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ '-]+$/).withMessage("name_Category_Inc must only contain alphanumeric characters, spaces and specific accented characters.")
     .isLength({ max: 45 }).withMessage("name_Category_Inc must have a maximum of 45 characters"),
 
     body("description_Category_Inc")
     .notEmpty().withMessage("description_Category_Inc must not be empty")
     .isString().withMessage("description_Category_Inc must be a string")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.\s]+$/).withMessage("description_Category_Inc must only contain alphanumeric characters, spaces and specific accented characters.")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.\s'"!?-]+$/).withMessage("description_Category_Inc must only contain alphanumeric characters, spaces and specific accented characters.")
     .isLength({ max: 600 }).withMessage("description_Category_Inc must have a maximum of 600 characters")
 ]
--- api/controllers/vClassroom.js
+++ api/controllers/vClassroom.js
@@ -5,13 +5,13 @@ export const vClassroom = [
     body("name_Classroom")
     .notEmpty().withMessage("name_Classroom must not be empty")
     .isString().withMessage("name_Classroom must be a string")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]+$/).withMessage("name_Classroom must only contain alphanumeric characters, spaces and specific accented characters.")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ '-]+$/).withMessage("name_Classroom must only contain alphanumeric characters, spaces and specific accented characters.")
     .isLength({ max: 45 }).withMessage("name_Classroom must have a maximum of 45 characters"),
 
     body("description_Classroom")
     .notEmpty().withMessage("description_Classroom must not be empty")
     .isString().withMessage("description_Classroom must be a string")
-    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.\s]+$/).withMessage("description_Classroom must only contain alphanumeric characters, spaces and specific accented characters.")
+    .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ,.\s'"!?-]+$/).withMessage("description_Classroom must only contain alphanumeric characters, spaces and specific accented characters.")
     .isLength({ max: 600 }).withMessage("description_Classroom must have a maximum of 600 characters"),
 
     body("classroom_Area")
