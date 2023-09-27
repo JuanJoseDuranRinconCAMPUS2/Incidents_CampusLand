ec99b7fdb1f73f76a17c8fc03a80df4ba0c2e580
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-26 22:33:37 -0500

    feat: :sparkles: Endpoint route controller: UserCreation
    the path control file is created for the endpoint in question where the different utilities per version are located.

---
+++ api/routes/userCreation.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import routesVersioning  from 'express-routes-versioning';
+import { proxyUserCreation, proxyUserDeletion } from "../middleware/proxyPEndpoints.js";
+import { proxyCreateUser, proxyDeleteUser } from "../middleware/proxyCreateUser.js";
+import { UserCreationv110, getUsersNotAuthorizedV110, deleteUserNotAuthorizedV110 } from "../versions/V1.1.0/userCreationv1.1.0.js";
+import { getLimit, postAndPutLimit , deleteLimit } from '../middleware/rateLimit.js';
+import { validatorIds } from "../controllers/vId.js";
+
+const UserCreation = Router();
+const version = routesVersioning();
+
+UserCreation.get('/UsersNotAuthorized', getLimit(), validatorIds, version({
+    "1.1.0": getUsersNotAuthorizedV110
+}))
+
+UserCreation.post('/', postAndPutLimit(900), proxyUserCreation, proxyCreateUser, version({
+    "1.1.0": UserCreationv110
+}))
+
+UserCreation.delete('/deleteUserNotAuthorized', postAndPutLimit(120), proxyUserDeletion, proxyDeleteUser, version({
+    "1.1.0": deleteUserNotAuthorizedV110
+}))
+
+
+export default UserCreation;
