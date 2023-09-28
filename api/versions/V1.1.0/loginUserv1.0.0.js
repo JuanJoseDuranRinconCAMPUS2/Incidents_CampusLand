41958fbabe6fde4de5e0a800eae40f7bad6ddb58
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-27 20:44:10 -0500

    fix: :art: grammar solutions
    grammar solutions

--- api/versions/V1.1.0/loginUserv1.0.0.js
+++ api/versions/V1.1.0/loginUserv1.0.0.js
@@ -10,26 +10,25 @@ let Tokens_Api = db.collection("Tokens_Api");
 export const LoginUserV110 = async (req, res) =>{
     if(!req.rateLimit) return;
     try {
-        let data = req.body
-        const encoder = new TextEncoder();
-        const jwtconstructor = new SignJWT(Object.assign({}, data));
-        const jwt = await jwtconstructor
-        .setProtectedHeader({alg:"HS256", typ: "JWT"})
-        .setIssuedAt()
-        .setExpirationTime("2h")
-        .sign(encoder.encode(env.NODE_JWT_PRIMATE_KEY));
-        req.data = jwt;
-     
-        let update = await Tokens_Api.findOne({ Id_User : data.id_User });
+          let data = req.body
+          const encoder = new TextEncoder();
+          const jwtconstructor = new SignJWT(Object.assign({}, data));
+          const jwt = await jwtconstructor
+          .setProtectedHeader({alg:"HS256", typ: "JWT"})
+          .setIssuedAt()
+          .setExpirationTime("2h")
+          .sign(encoder.encode(env.NODE_JWT_PRIMATE_KEY));
+          req.data = jwt;
+          let update = await Tokens_Api.findOne({ Id_User_Api : data.id_User });
 
-        (!update)
-        ?   await Tokens_Api.insertOne({ Id_User : data.id_User , Token : jwt })
-        :   await Tokens_Api.findOneAndUpdate({ Id_User : data.id_User },{$set: { Token : jwt }}); 
-        
-        res.status(201).send({status: 201, message: jwt, instructions: "En el header de la peticion pon el header 'Authorization' y luego pon esta llave como valor"});
+          (!update)
+          ?   await Tokens_Api.insertOne({ Id_User_Api : data.id_User , Token : jwt })
+          :   await Tokens_Api.findOneAndUpdate({ Id_User_Api : data.id_User },{$set: { Token : jwt }}); 
+          
+          res.status(201).send({status: 201, message: jwt, instructions: "In the header of the request put the header 'Authorization' and then put this key as value"});
   } catch (error) {
     console.log(error);
-      res.status(404).send({status: 404, message: "Error al crear el Token"});
+      res.status(404).send({status: 404, message: "Error when creating the token"});
   }
 
 }
