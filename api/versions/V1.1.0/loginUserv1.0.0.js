451041ee4fbb58b7b08d9f951f3a6e5b04aaa9e2
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-27 21:53:34 -0500

    feat: :sparkles: the user id is added as login response
    the user id is added as login response

--- api/versions/V1.1.0/loginUserv1.0.0.js
+++ api/versions/V1.1.0/loginUserv1.0.0.js
@@ -25,7 +25,7 @@ export const LoginUserV110 = async (req, res) =>{
           ?   await Tokens_Api.insertOne({ Id_User_Api : data.id_User , Token : jwt })
           :   await Tokens_Api.findOneAndUpdate({ Id_User_Api : data.id_User },{$set: { Token : jwt }}); 
           
-          res.status(201).send({status: 201, message: jwt, instructions: "In the header of the request put the header 'Authorization' and then put this key as value"});
+          res.status(201).send({status: 201, message: jwt, instructions: "In the header of the request put the header 'Authorization' and then put this key as value", User : data.id_User });
   } catch (error) {
     console.log(error);
       res.status(404).send({status: 404, message: "Error when creating the token"});
