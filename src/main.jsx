e4fd88feebea498f4467c6b6d75a0b3eb29a5935
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-29 22:07:26 -0500

    feat: :sparkles: home element import
    the first path of the frontend is created, which is the welcome page for the user.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import Home from './components/home/Home';
 
+const root = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+   
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let app = document.querySelector('#root')
+ReactDOM.createRoot(app).render(
   <React.StrictMode>
-  
+    <RouterProvider router={root}/>
   </React.StrictMode>,
 )
