759724231b5ccb48e7ee2445356c2972a715b8ce
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-30 13:15:25 -0500

    feat: :sparkles: implementation of the error404 component in the project paths
    the error component is implemented for any route other than one assigned in the project.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
 import Home from './components/home/Home';
+import Error404 from './components/error404';
 
 const root = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
    
+  },
+
+  {
+    path: "*",
+    element: <Error404/>,
   },
 ]);
 
