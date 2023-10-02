71853c2bfd1e18be131010ba65d2463fdcb2982f
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-10-02 18:08:52 -0500

    feat: :alien: api connection file
    the function to connect the api and its user creation path with the frontend is created.

---
+++ src/services/UserCreation.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+let url = `http://${import.meta.env.VITE_HOSTNAME}:${import.meta.env.VITE_PORT_BACKEND}/UserCreation`
+
+export let UserCreation = async (User, onProgress)=>{
+    try{
+        const response = await axios.post(url, User, {
+            headers: {
+                'content-Type': 'application/json',
+                'Accept-Version': '1.1.0'
+            },
+            onUploadProgress: (progressEvent) => {
+                const progress = (progressEvent.loaded / progressEvent.total) * 100;
+                onProgress(progress);
+            }
+        });
+        return (response);      
+    }catch(error){
+        return (error.response);
+    }
+}
