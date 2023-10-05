ed052bb69ed6b5ce1282c589f50363ed22394404
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-10-05 02:51:59 -0500

    feat: :alien: user validation management endpoints
    the api connection functions are created to manage the endpoints in charge of the validation management of the users registered to the api.

--- src/services/UserCreation.jsx
+++ src/services/UserCreation.jsx
@@ -19,3 +19,60 @@ export let UserCreation = async (User, onProgress)=>{
         return (error.response);
     }
 }
+
+export let getUsersNotAuthorized = async (onProgress, token)=>{
+    try{
+        const response = await axios.get(`${url}/UsersNotAuthorized`, {
+            headers: {
+                'content-Type': 'application/json',
+                'Accept-Version': '1.2.0',
+                'Authorization' : token,
+            },
+            onUploadProgress: (progressEvent) => {
+                const progress = (progressEvent.loaded / progressEvent.total) * 100;
+                onProgress(progress);
+            }
+        });
+        return (response);      
+    }catch(error){
+        return (error.response);
+    }
+}
+
+export let deleteUserNotAuthorized = async (User, onProgress, token)=>{
+    try{
+        const response = await axios.post(`${url}/deleteUserNotAuthorized`, User, {
+            headers: {
+                'content-Type': 'application/json',
+                'Accept-Version': '1.0.0',
+                'Authorization' : token,
+            },
+            onUploadProgress: (progressEvent) => {
+                const progress = (progressEvent.loaded / progressEvent.total) * 100;
+                onProgress(progress);
+            }
+        });
+        return (response);      
+    }catch(error){
+        return (error.response);
+    }
+}
+
+export let authorizeUsers = async (User, onProgress, token)=>{
+    try{
+        const response = await axios.post(`${url}/authorizeUsers`, User, {
+            headers: {
+                'content-Type': 'application/json',
+                'Accept-Version': '1.0.0',
+                'Authorization' : token,
+            },
+            onUploadProgress: (progressEvent) => {
+                const progress = (progressEvent.loaded / progressEvent.total) * 100;
+                onProgress(progress);
+            }
+        });
+        return (response);      
+    }catch(error){
+        return (error.response);
+    }
+}
